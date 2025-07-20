5a87b459fdd835af750d6c0c375e24d08bf7e8f3
Sudheer Reddy <sudheerjanga9999@gmail.com>
2025-07-20 10:21:45 +0530

    removed chat-bot code

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,13 +4,9 @@ import express from "express";
 import AuthController from "../controllers/auth.controller.js";
 import UserController from "../controllers/user.controller.js";
 import OtpController from "../controllers/otp.contoller.js";
-import ScrapingController from "../controllers/chat/scrape.controller.js";
 import UserJwtMiddleWare from "../middlewares/jwt..usermiddleware.js";
 import customUtility from "../utility/custom.utility.js";
 import AuthValidation from "../middlewares/validators/auth.validation.js";
-import ScrapingValidation from "../middlewares/validators/scrape.validation.js";
-import ChatController from "../controllers/chat/chat.controller.js";
-import EmbedController from "../controllers/chat/embed.controller.js";
 
 const Router = express.Router();
 
@@ -25,13 +21,6 @@ Router.post("/google/login", AuthController.GetGoogleAuthController);
 Router.post("/register", UserController.RegisterNewUserController);
 Router.get("/registerOtp", OtpController.InsertOtp);
 Router.get("/verifyOtp", AuthController.VerifyOtpController);
-Router.post(
-  "/scrape",
-  ScrapingValidation.scrapeValidation,
-  ScrapingController.ScrapeController
-);
-Router.post("/chat", ChatController.QueryServicesController);
-Router.post("/embed", EmbedController.EmbedAndServiceController);
 
 Router.use(UserJwtMiddleWare.VerifyToken);
 Router.get("/details", UserController.GetUserDetailsContoller);
